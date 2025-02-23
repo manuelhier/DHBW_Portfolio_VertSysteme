5e79bdfdf2589528e635bef23cdd24830178b01c
Manuel Rettig <manuel.rettig@mail.de>
2025-02-23 13:13:50 +0100

    Adjust controller and add jsdoc documentation

--- smart-home-controller/src/routes/device.controller.js
+++ smart-home-controller/src/routes/device.controller.js
@@ -1,27 +1,164 @@
 import express from "express";
 import logging from "logging";
+import DeviceInputModel from "../model/device.model.js";
 
 const logger = logging.default("device-controller");
 const deviceController = express.Router();
 
 // Device Collection Endpoints
+
+/**
+ * @openapi
+ * /api/v1/device:
+ *   get:
+ *     tags:
+ *       - Device
+ *     summary: Get all devices
+ *     description: Get all devices
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Device'
+ *       404:
+ *         description: Not Found
+ *       400:
+ *         description: Bad Request
+ */
 deviceController.get('/device', getDevice);
+
+/**
+ * @openapi
+ * /api/v1/device:
+ *   post:
+ *     tags:
+ *       - Device
+ *     summary: Create a new device
+ *     description: Create a new device
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/DeviceInput'
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Device'
+ *       400:
+ *         description: Bad Request
+ */
 deviceController.post('/device', createDevice);
 
 // Device Ressource Endpoints
-deviceController.get('/device/:id', getDeviceById);
-deviceController.patch('/device/:id', updateDeviceById);
-deviceController.delete('/device/:id', deleteDeviceById);
 
+/**
+ * @openapi
+ * /api/v1/device/{id}:
+ *   get:
+ *     tags:
+ *       - Device
+ *     summary: Get a device by ID
+ *     description: Get a device by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The device ID
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Device'
+ *       404:
+ *         description: Not Found
+ *       400:
+ *         description: Bad Request
+ */
+deviceController.get('/device/:id', getDeviceById);
+
+/**
+ * @openapi
+ * /api/v1/device/{id}:
+ *   patch:
+ *     tags:
+ *       - Device
+ *     summary: Update a device by ID
+ *     description: Update a device by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The device ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/DeviceUpdate'
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Device'
+ *       404:
+ *         description: Not Found
+ *       400:
+ *         description: Bad Request
+ */
+deviceController.patch('/device/:id', updateDeviceById);
+
+/**
+ * @openapi
+ * /api/v1/device/{id}:
+ *   delete:
+ *     tags:
+ *       - Device
+ *     summary: Delete a device by ID
+ *     description: Delete a device by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The device ID
+ *     responses:
+ *       200:
+ *         description: Success
+ *       404:
+ *         description: Not Found
+ *       400:
+ *         description: Bad Request
+ */
+deviceController.delete('/device/:id', deleteDeviceById);
 
 function getDevice(_req, res) {
     logger.info("Getting all devices");
     res.send("Getting all devices");
 }
 
-function createDevice(_req, res) {
+function createDevice(req, res) {
+
+    const device = new DeviceInputModel(req.body);
+
     logger.info("Creating a device");
-    res.send("Creating a device");
+    res.send(device);
 }
 
 function getDeviceById(req, res) {
--- smart-home-controller/src/routes/user.controller.js
+++ smart-home-controller/src/routes/user.controller.js
@@ -5,12 +5,146 @@ const logger = logging.default("user-controller");
 const userController = express.Router();
 
 // User Collection Endpoints
+
+/**
+ * @openapi
+ * /api/v1/user:
+ *   get:
+ *     tags:
+ *       - User
+ *     summary: Get all users
+ *     description: Get all users
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *       404:
+ *         description: Not Found
+ *       400:
+ *         description: Bad Request
+ */
 userController.get('/user', getUser);
+
+/**
+ * @openapi
+ * /api/v1/user:
+ *   post:
+ *     tags:
+ *       - User
+ *     summary: Create a new user
+ *     description: Create a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserInput'
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Bad Request
+ */
 userController.post('/user', createUser);
 
 // User Ressource Endpoints
+
+/**
+ * @openapi
+ * /api/v1/user/{id}:
+ *   get:
+ *     tags:
+ *       - User
+ *     summary: Get a user by ID
+ *     description: Get a user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: Not Found
+ *       400:
+ *         description: Bad Request
+ */
 userController.get('/user/:id', getUserById);
+
+/**
+ * @openapi
+ * /api/v1/user/{id}:
+ *   patch:
+ *     tags:
+ *       - User
+ *     summary: Update a user by ID
+ *     description: Update a user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserInput'
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: Not Found
+ *       400:
+ *         description: Bad Request
+ */
 userController.patch('/user/:id', updateUserById);
+
+/**
+ * @openapi
+ * /api/v1/user/{id}:
+ *   delete:
+ *     tags:
+ *       - User
+ *     summary: Delete a user by ID
+ *     description: Delete a user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: Success
+ *       404:
+ *         description: Not Found
+ *       400:
+ *         description: Bad Request
+ */
 userController.delete('/user/:id', deleteUserById);
 
 function getUser(_req, res) {
