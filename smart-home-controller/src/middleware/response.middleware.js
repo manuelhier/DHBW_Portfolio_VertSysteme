82deb8870bf6d950fab0101666bdf428703b9b30
Manuel Rettig <manuel.rettig@mail.de>
2025-03-05 14:30:46 +0100

    Adjust error middleware response format

--- smart-home-controller/src/middleware/response.middleware.js
+++ smart-home-controller/src/middleware/response.middleware.js
@@ -2,15 +2,16 @@ import logging from 'logging';
 
 const logger = logging.default('ERROR');
 
-function addErrorHandlerMiddleware (err, req, res, next) {
-    const errStatus = err.status || 500;
+function addErrorHandlerMiddleware(err, req, res, next) {
+    const errStatus = err.status || err.name === 'ValidationError' ? 400 : 500;
     const errMessage = err.message || 'Internal Server Error';
 
     logger.error(err.stack);
     res.status(errStatus).json({
-        success: false,
-        status: errStatus,
-        message: errMessage,
+        error: {
+            status: errStatus,
+            message: errMessage,
+        }
     });
 
     next();
