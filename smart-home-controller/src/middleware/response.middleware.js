5b22631f12537a2b7ef4f361df961a49d1dd802c
Manuel Rettig <manuel.rettig@mail.de>
2025-03-26 15:44:51 +0100

    Improve response middleware

--- smart-home-controller/src/middleware/response.middleware.js
+++ smart-home-controller/src/middleware/response.middleware.js
@@ -3,8 +3,8 @@ import logging from 'logging';
 const logger = logging.default('ERROR');
 
 function addErrorHandlerMiddleware(err, req, res, next) {
-    const errStatus = err.status || err.name === 'ValidationError' ? 400 : 500;
-    const errMessage = err.message || 'Internal Server Error';
+    const errStatus = err.status ? err.status : 500;
+    const errMessage = err.message ? err.message : 'Internal Server Error';
 
     logger.error(err.stack);
     res.status(errStatus).json({
