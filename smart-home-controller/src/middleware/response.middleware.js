989066f38d076e14bb06b11c7b092a7eed1e429d
Manuel Rettig <manuel.rettig@mail.de>
2025-03-28 18:25:43 +0100

    Update error handling middleware to log warnings instead of errors

--- smart-home-controller/src/middleware/response.middleware.js
+++ smart-home-controller/src/middleware/response.middleware.js
@@ -2,15 +2,15 @@ import logging from 'logging';
 
 const logger = logging.default('ERROR');
 
-function addErrorHandlerMiddleware(err, req, res, next) {
+function addErrorHandlerMiddleware(err, _req, res, next) {
     const errStatus = err.status ? err.status : 500;
     const errMessage = err.message ? err.message : 'Internal Server Error';
 
-    logger.error(err.stack);
+    logger.warn(err.stack);
     res.status(errStatus).json({
         error: {
             status: errStatus,
-            message: errMessage,
+            message: errMessage
         }
     });
 
