6e0911313efc2f0a218fdbc92b9f28c8618cd03e
Manuel Rettig <manuel.rettig@mail.de>
2025-03-31 22:25:59 +0200

    Disable warnings

--- smart-home-controller/src/middleware/response.middleware.js
+++ smart-home-controller/src/middleware/response.middleware.js
@@ -6,7 +6,7 @@ function addErrorHandlerMiddleware(err, _req, res, next) {
     const errStatus = err.status ? err.status : 500;
     const errMessage = err.message ? err.message : 'Internal Server Error';
 
-    logger.warn(err.stack);
+    // logger.warn(err.stack);
     res.status(errStatus).json({
         error: {
             status: errStatus,
